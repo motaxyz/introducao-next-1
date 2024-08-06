03937ac6c5a2f5098d593e19c8afc3963cc91509
Nei Junio Nogueira Gomes <152228340+NeiJunio@users.noreply.github.com>
2024-08-06 20:40:20 -0300

    commit

---
+++ src/app/sobre/page.jsx
@@ -0,0 +1,9 @@
+import styles from './page.module.css'
+
+export default function Sobre() {
+    return(
+        <div className="containerGlobal">
+            <h1 className={styles.titleSobre}>Sobre</h1>
+        </div>
+    )
+}
---
+++ src/app/usuarios/cadastro/page.jsx
@@ -0,0 +1,8 @@
+
+export default function CadastroUsuario() {
+    return(
+        <div className="containerGlobal">
+            <h1>Cadastro de usuário</h1>
+        </div>
+    )
+}
