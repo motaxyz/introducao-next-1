9ae6c23d9e9dfa11b79058247de66a1abf07eeff
Matheus Mota Tonini <138072866+motaxyz@users.noreply.github.com>
2024-08-16 20:17:06 -0300

    commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,11 +2,12 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 import Rodape from "@/components/rodape";
+import Cabecalho from "@/components/cabecalho";
 
 
 const inter = Inter({ subsets: ["latin"] });
 
-import Rodape from "@/components/rodape";
+
 
 
 export const metadata = {
@@ -18,10 +19,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+      <Cabecalho />
       {children}
+      
       <Rodape />
-        {children}
-        <Rodape />
       </body>
     </html>
   );
